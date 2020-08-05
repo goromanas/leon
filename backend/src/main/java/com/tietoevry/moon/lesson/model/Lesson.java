ce442b510cbbd37de42cf8ef0df02d7aa2487152
jtalacka <julius.talacka@gmail.com>
2020-08-05 16:26:46 +0300

    Added subject and partially Lesson

---
+++ backend/src/main/java/com/tietoevry/moon/lesson/model/Lesson.java
@@ -0,0 +1,25 @@
+package com.tietoevry.moon.lesson.model;
+
+import com.tietoevry.moon.subject.model.Subject;
+import com.tietoevry.moon.user.model.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Lesson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Subject subject;
+    private User teacher;
+    private String feed;
+    private String day;
+    private int time;
+}
