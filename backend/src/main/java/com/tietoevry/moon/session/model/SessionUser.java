f1d94f262145153de6718733ba6ff348d55e0010
jtalacka <julius.talacka@gmail.com>
2020-08-05 14:02:33 +0300

    Added list of roles to getSession

--- backend/src/main/java/com/tietoevry/moon/session/model/SessionUser.java
+++ backend/src/main/java/com/tietoevry/moon/session/model/SessionUser.java
@@ -2,12 +2,16 @@ package com.tietoevry.moon.session.model;
 
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class SessionUser {
 
     private String username;
+    private List<String> roles;
 
-    public SessionUser(String username) {
+    public SessionUser(String username, List<String> roles) {
         this.username = username;
+        this.roles = roles;
     }
 }
