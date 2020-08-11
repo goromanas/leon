cfbd3da3a8a052b6e27267eb9b6aa22599f124a8
Zymantas <kyssaz@gmail.com>
2020-08-11 14:34:42 +0300

    fe socket test

--- frontend/src/app/page/home/socket-test.tsx
+++ frontend/src/app/page/home/socket-test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // @ts-ignore
 import Websocket from 'react-websocket';
 
-interface State { currentLesson?: number }
+interface State { currentLesson?: string }
 interface OwnProps {
 }
 
@@ -13,22 +13,26 @@ class CurrentLesson extends React.Component<Props, State> {
     constructor(props: any) {
         super(props);
         this.state = {
-            currentLesson: 1
+            currentLesson: "test"
         };
     }
 
     handleData(data: any) {
         let result = JSON.parse(data);
-        this.setState({currentLesson: result});
+        // this.setState({currentLesson: data});
+        console.log(result)
     }
+
 
     render() {
         return (
             <div>
                 CurrentLesson <strong>{this.state.currentLesson}</strong>
 
-                <Websocket url='ws://localhost:8000/currentLesson/'
-                           onMessage={this.handleData.bind(this)}/>
+                <Websocket url='ws://localhost:3000/currentLesson'
+                           onMessage={this.handleData.bind(this)}
+                           debug={true}/>
+
             </div>
         );
     }
