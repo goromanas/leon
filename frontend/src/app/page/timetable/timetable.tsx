adb2142a5b62c38208de7a5071da8b83c0bfedc3
jtalacka <julius.talacka@gmail.com>
2020-08-13 17:10:55 +0300

    fixed backend error for timetable lessonInformation

--- frontend/src/app/page/timetable/timetable.tsx
+++ frontend/src/app/page/timetable/timetable.tsx
@@ -48,26 +48,29 @@ class TimetablePageComponent extends React.Component<Props, State> {
 
                     {Array(5).fill(new Date().getDay() + this.state.move).map((x, y) => x + y).map((item) => (
                         item == 0 ? item = 5 : null,
-                        item < 0 ? item = 0 - item : null,
+                            item < 0 ? item = 0 - item : null,
                             (item % 5) != 0 ? null : item = 5,
                             (item % 5) != 0 ? item = item % 5 : null,
 
-                    <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 1 }}>
-                        <Lessons lessonsList={this.filterByDay(teacherLessons, item) || []} day={item} time={this.getDate()} />
-                    </Col>
-                        ))}
+                            <Col xs={{span: 5, offset: 1}} lg={{span: 6, offset: 1}}>
+                                <Lessons lessonsList={this.filterByDay(teacherLessons, item) || []} day={item}
+                                         time={this.getDate()}/>
+                            </Col>
+                    ))}
                 </Row>,
 
             </>);
     }
+
     public filterByDay(teacherLessons: Api.Lesson[], day: number): Api.Lesson[] {
 
         if (teacherLessons != null) {
             this.sortedLesson = teacherLessons.sort((n1, n2) => n1.time - n2.time);
-
             return this.sortedLesson.filter(lesson => lesson.day == day ? lesson : null);
 
-        }}
+        }
+    }
+
     public getDate = (): String => {
         const today = new Date();
         const day = moment().add(this.state.move, 'd').format('YYYY-MM-DD');
@@ -77,14 +80,14 @@ class TimetablePageComponent extends React.Component<Props, State> {
     private handleButtonClick = (forward: boolean): void => {
 
         forward ?
-        this.setState({ move: this.state.move + 1 }) :
-        this.setState({ move: this.state.move - 1 });
+            this.setState({move: this.state.move + 1}) :
+            this.setState({move: this.state.move - 1});
 
     };
 
 }
 
-const mapContextToProps = ({ session: { user }, lessons }: SettingsProps): ContextProps => ({
+const mapContextToProps = ({session: {user}, lessons}: SettingsProps): ContextProps => ({
 
     username: user != null ? user.username : null,
     userRoles: user.roles,
