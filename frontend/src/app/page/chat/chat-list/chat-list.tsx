20532bbd299cb5d0bac2a8219419e9cf58348815
Romanas <go.romanas@gmail.com>
2020-08-17 08:53:43 +0300

    Chat channels in FE

--- frontend/src/app/page/chat/chat-list/chat-list.tsx
+++ frontend/src/app/page/chat/chat-list/chat-list.tsx
@@ -1,33 +1,34 @@
-import React from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { message } from 'antd';
 
-import { Message } from "./message";
-import { connectContext, SettingsProps } from "app/context";
-import { message } from "antd";
+import { connectContext, SettingsProps } from 'app/context';
+
+import { Message } from './message';
 
 interface Message {
-  text: string;
-  author: string;
-  date: string;
-  classroom?: string;
-  subject?: number;
-  channel?: number;
+    text: string;
+    author: string;
+    date: string;
+    classroom?: string;
+    subject?: number;
+    channel?: number;
 }
 
 interface ContextProps {
-  teacherLessons: Api.Lesson[];
+    teacherLessons: Api.Lesson[];
 }
 
 interface OwnProps {
-  messages: Message[];
-  currentChannel: number;
+    messages: Message[];
+    currentChannel: number;
 }
 type Props = ContextProps & OwnProps;
 
 const ChatListComponent: React.FC<Props> = ({ messages, currentChannel }) => (
   <div>
     <ul>
-      <TransitionGroup className="chat-messages" appear>
+      <TransitionGroup className="chat-messages" appear={true}>
         {messages
           .filter(message => message.channel === currentChannel)
           .map((msg, i) => (
@@ -41,7 +42,7 @@ const ChatListComponent: React.FC<Props> = ({ messages, currentChannel }) => (
 );
 
 const mapContextToProps = ({ lessons }: SettingsProps): ContextProps => ({
-  teacherLessons: lessons
+    teacherLessons: lessons,
 });
 
 const ChatList = connectContext(mapContextToProps)(ChatListComponent);
--- frontend/src/app/page/chat/chat-page.tsx
+++ frontend/src/app/page/chat/chat-page.tsx
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 import { Layout } from "antd";
 import { FormikHelpers } from "formik";
+import Sider from "antd/lib/layout/Sider";
 
 import { PageContent } from "app/components/layout";
 import { connectContext, SettingsProps } from "app/context";
+import { chatService } from "app/api/service/chat-service";
 
 import { ChatForm, MessageValue } from "./form/chat-form";
 import { ChatList } from "./chat-list/chat-list";
 import { Channels } from "./channels";
-import Sider from "antd/lib/layout/Sider";
-import { chatService } from "app/api/service/chat-service";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const { Content } = Layout;
 
 interface ContextProps {
-  username: string | null;
-  teacherLessons: Api.Lesson[];
+    username: string | null;
+    teacherLessons: Api.Lesson[];
 }
 
 interface OwnProps {}
@@ -23,82 +24,83 @@ interface OwnProps {}
 type Props = ContextProps & OwnProps;
 
 interface Message {
-  text: string;
-  author: string;
-  date: string;
-  classroom?: string;
-  subject?: number;
-  channel?: number;
+    text: string;
+    author: string;
+    date: string;
+    classroom?: string;
+    subject?: number;
+    channel?: number;
 }
 
 interface State {
-  messages: Message[];
-  file: any;
-  className: string | null;
-  lessonId: number | null;
-  channels: Api.Subject[];
-  currentChannel: number | null;
+    messages: Message[];
+    file: any;
+    className: string | null;
+    lessonId: number | null;
+    channels: Api.Subject[];
+    currentChannel: number | null;
 }
 
 class ChatComponent extends React.Component<Props, State> {
-  public getSocketUrl = (): string => {
-    const loc = window.location;
-    let newUrl: string;
-    if (loc.host === "localhost:3000") {
-      newUrl = "ws://localhost:8080/ws/chat";
-    } else {
-      newUrl = " wss://java-menuo-su-it.northeurope.cloudapp.azure.com/ws/chat";
-    }
-    return newUrl;
-  };
+    public getSocketUrl = (): string => {
+        const loc = window.location;
+        let newUrl: string;
 
-  public ws = new WebSocket(this.getSocketUrl());
+        if (loc.host === 'localhost:3000') {
+          newUrl = 'ws://localhost:8080/ws/chat';
+      } else {
+          newUrl = ' wss://java-menuo-su-it.northeurope.cloudapp.azure.com/ws/chat';
+      }
+        return newUrl;
+    };
 
-  public readonly state: State = {
-    messages: [],
-    file: null,
-    className: null,
-    lessonId: null,
-    channels: [],
-    currentChannel: 1
-  };
-  public static MESSAGE_INITIAL_VALUES: MessageValue = { message: "" };
+    public ws = new WebSocket(this.getSocketUrl());
+
+    public readonly state: State = {
+        messages: [],
+        file: null,
+        className: null,
+        lessonId: null,
+        channels: [],
+        currentChannel: 1,
+    };
+    public static MESSAGE_INITIAL_VALUES: MessageValue = { message: '' };
 
   // tslint:disable-next-line:typedef
-  public componentDidMount() {
-    const { messages } = this.state;
-    const { teacherLessons } = this.props;
-    const currentChannel: number = 1;
+    public componentDidMount() {
+        const { messages } = this.state;
+        const { teacherLessons } = this.props;
+        const currentChannel: number = 1;
 
-    console.log(teacherLessons);
+        console.log(teacherLessons);
     // this.ws.onopen = () => {
     //     console.log('connected');
     // };
-    if (this.state.channels.length < 1) {
-      chatService
+        if (this.state.channels.length < 1) {
+          chatService
         .getSubjects()
         .then(channels => this.setState({ channels }))
-        .catch(() => console.log("Error getting subjects"));
-    }
+        .catch(() => console.log('Error getting subjects'));
+      }
 
-    this.state.channels && console.log(this.state.channels);
+        this.state.channels && console.log(this.state.channels);
 
-    this.ws.onmessage = e => {
-      const message = JSON.parse(e.data);
+        this.ws.onmessage = e => {
+          const message = JSON.parse(e.data);
       // console.log('Chat page receives ',message.classroom);
 
-      const copyMsg = [...this.state.messages];
-      const newMsg = [...copyMsg, message];
+          const copyMsg = [...this.state.messages];
+          const newMsg = [...copyMsg, message];
 
-      this.setState({
-        messages: newMsg
-      });
-    };
-  }
+          this.setState({
+            messages: newMsg,
+        });
+      };
+    }
 
-  public render(): React.ReactNode {
-    const { messages, channels } = this.state;
-    const { teacherLessons } = this.props;
+    public render(): React.ReactNode {
+        const { messages, channels } = this.state;
+        const { teacherLessons } = this.props;
 
     // if (teacherLessons) {
     //   console.log(teacherLessons);
@@ -117,14 +119,14 @@ class ChatComponent extends React.Component<Props, State> {
     //   // console.log(removedDuplicates)
     // }
 
-    return (
+        return (
       <Layout>
         <Sider>
           <Channels channels={channels} currentChannel={this.state.currentChannel} onChannelChange={this.onChannelChange} />
         </Sider>
         <Content>
           <PageContent>
-            <ChatList messages={messages} currentChannel={this.state.currentChannel} />
+         <ChatList messages={messages} currentChannel={this.state.currentChannel} />
             <ChatForm
               initialValues={ChatComponent.MESSAGE_INITIAL_VALUES}
               onSubmit={this.handleSubmit}
@@ -133,22 +135,22 @@ class ChatComponent extends React.Component<Props, State> {
           </PageContent>
         </Content>
       </Layout>
-    );
-  }
+      );
+    }
 
   // public addFile = (file: any) => {
   //     this.setState({ file: file });
   //     console.log(file);
   // }
 
-  public sendMessage = (message: Message) => {
-    try {
+    public sendMessage = (message: Message) => {
+        try {
       // console.log(message)
-      this.ws.send(JSON.stringify(message));
-    } catch (error) {
-      console.log(error); // catch error
-    }
-  };
+          this.ws.send(JSON.stringify(message));
+      } catch (error) {
+          console.log(error); // catch error
+      }
+    };
 
   // if (teacherLessons) {
   //   console.log(teacherLessons);
@@ -159,50 +161,50 @@ class ChatComponent extends React.Component<Props, State> {
   //     console.log(file);
   // }
 
-  private readonly handleSubmit = (values: MessageValue, { resetForm }: FormikHelpers<MessageValue>): void => {
-    const { messages, currentChannel } = this.state;
-    const { teacherLessons } = this.props;
-    const time = new Date();
-    const hours = time.getHours().toString();
-    const minutes = time.getMinutes().toString();
+    private readonly handleSubmit = (values: MessageValue, { resetForm }: FormikHelpers<MessageValue>): void => {
+        const { messages, currentChannel } = this.state;
+        const { teacherLessons } = this.props;
+        const time = new Date();
+        const hours = time.getHours().toString();
+        const minutes = time.getMinutes().toString();
     // console.log(Object.keys(this.props.teacherLessons[0]));
     // console.log(this.props.teacherLessons[0]["className"]);
 
     // @ts-ignore
-    const className: string = this.props.teacherLessons[0].className;
+        const className: string = this.props.teacherLessons[0].className;
 
     // console.log(teacherLessons);
     // const id: number = this.props.teacherLessons
     // console.log(values.message, messages[0].channel);
-    if (values.message.trim() !== "") {
-      this.setState({
-        messages: [...messages, { text: values.message, author: this.props.username, date: hours + ":" + minutes, channel: currentChannel }],
-        className
-      });
-      this.sendMessage({
-        text: values.message,
-        author: this.props.username,
-        date: hours + ":" + minutes,
-        classroom: className,
-        channel: currentChannel
+        if (values.message.trim() !== '') {
+          this.setState({
+            messages: [...messages, { text: values.message, author: this.props.username, date: hours + ':' + minutes, channel: currentChannel }],
+            className,
+        });
+          this.sendMessage({
+            text: values.message,
+            author: this.props.username,
+            date: hours + ':' + minutes,
+            classroom: className,
+            channel: currentChannel,
         // id:
-      });
-    }
-    resetForm();
-    this.setState({ file: null });
-  };
+        });
+      }
+        resetForm();
+        this.setState({ file: null });
+    };
 
-  private readonly onChannelChange = (id: number): void => {
-    this.setState({
-      currentChannel: id
-    });
-    console.log(this.state.currentChannel);
-  };
+    private readonly onChannelChange = (id: number): void => {
+        this.setState({
+          currentChannel: id,
+      });
+        console.log(this.state.currentChannel);
+    };
 }
 
 const mapContextToProps = ({ session: { user }, lessons }: SettingsProps): ContextProps => ({
-  username: user != null ? user.username : null,
-  teacherLessons: lessons
+    username: user != null ? user.firstName : null,
+    teacherLessons: lessons,
 });
 
 const ChatPage = connectContext(mapContextToProps)(ChatComponent);
