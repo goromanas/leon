74b82f110d672ade21071f12df732eddb095b435
Romanas <go.romanas@gmail.com>
2020-08-19 22:05:12 +0300

    Fixed redirect after login issue to Home

--- frontend/src/app/page/login/form/login-form.tsx
+++ frontend/src/app/page/login/form/login-form.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Field, Form, Formik, FormikConfig } from 'formik';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import React from "react";
+import { Field, Form, Formik, FormikConfig } from "formik";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
-import { InputField, PasswordInputField, CheckBoxField } from 'app/components/inputs';
-import { FormButton, SubmitButton } from 'app/components/buttons';
-import { FormErrors } from 'app/model/form-errors';
+import { InputField, PasswordInputField, CheckBoxField } from "app/components/inputs";
+import { FormButton, SubmitButton } from "app/components/buttons";
+import { FormErrors } from "app/model/form-errors";
 
-import { InputIcon } from './input-icon';
+import { InputIcon } from "./input-icon";
 
 export interface LoginValues {
-    username: string;
-    password: string;
-    checkbox: boolean;
+  username: string;
+  password: string;
+  checkbox: boolean;
 }
 
 export type LoginErrors = FormErrors<LoginValues>;
@@ -19,52 +19,26 @@ export type LoginErrors = FormErrors<LoginValues>;
 type Props = FormikConfig<LoginValues>;
 
 const LoginForm: React.FC<Props> = (props: Props) => {
-    const {
-        initialValues,
-        onSubmit,
-        validate,
-    } = props;
+  const { initialValues, onSubmit, validate } = props;
 
-    return (
-        <Formik
-            initialValues={initialValues}
-            onSubmit={onSubmit}
-            validate={validate}
-        >
-            {() => (
-                <Form>
-                    <label>Prisijungimo vardas</label>
-                    <Field
-                        component={InputField}
-                        name="username"
-                        placeholder="Vardas123"
-                        prefix={<InputIcon component={UserOutlined} />}
-                    />
-                    <label>Slaptažodis</label>
-                    <Field
-                        component={PasswordInputField}
-                        name="password"
-                        placeholder="*********"
-                        prefix={<InputIcon component={LockOutlined} />}
-                    />
-                    <div className="block">
-                        <Field
-                        type="checkbox"
-                        name="checkbox"
-                        prefix={<InputIcon component={LockOutlined} />}
-                    />
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit} validate={validate}>
+      {() => (
+        <Form>
+          <label>Username</label>
+          <Field component={InputField} name="username" placeholder="username" prefix={<InputIcon component={UserOutlined} />} />
+          <label>Password</label>
+          <Field component={PasswordInputField} name="password" placeholder="*********" prefix={<InputIcon component={LockOutlined} />} />
+          <div className="block">
+            <Field type="checkbox" name="checkbox" prefix={<InputIcon component={LockOutlined} />} />
 
-                        <label> Prisiminti mane</label>
-                    </div>
-                    <FormButton
-                        component={SubmitButton}
-                    >
-                        Prisijungti
-                    </FormButton>
-                </Form>
-            )}
-        </Formik>
-    );
+            <label>Remember me</label>
+          </div>
+          <FormButton component={SubmitButton}>Sign In</FormButton>
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export { LoginForm };
--- frontend/src/app/page/login/login-page.tsx
+++ frontend/src/app/page/login/login-page.tsx
@@ -1,110 +1,100 @@
-import React from 'react';
-import { Col, Layout, message, Row } from 'antd';
-import { FormikHelpers } from 'formik';
+import React from "react";
+import { Col, Layout, message, Row } from "antd";
+import { FormikHelpers } from "formik";
 // tslint:disable-next-line:no-submodule-imports
-import { FormikState } from 'formik/dist/types';
+import { FormikState } from "formik/dist/types";
 
-import { PageContent } from 'app/components/layout';
-import { connectContext, SettingsProps } from 'app/context';
-import { navigationService } from 'app/service/navigation-service';
-import { sessionService } from 'app/api/service/session-service';
-import { loggerService } from 'app/service/logger-service';
+import { PageContent } from "app/components/layout";
+import { connectContext, SettingsProps } from "app/context";
+import { navigationService } from "app/service/navigation-service";
+import { sessionService } from "app/api/service/session-service";
+import { loggerService } from "app/service/logger-service";
 
-import { LoginErrors, LoginForm, LoginValues } from './form/login-form';
+import { LoginErrors, LoginForm, LoginValues } from "./form/login-form";
 
-import styles from './login-page.module.scss';
+import styles from "./login-page.module.scss";
+import { Logo } from "app/components/logo/logo";
 
 const { Content } = Layout;
 
 interface ContextProps {
-    authenticated: boolean;
+  authenticated: boolean;
 }
 
-interface OwnProps {
-}
+interface OwnProps {}
 
 message.config({
-    duration: 2,
-    maxCount: 1,
+  duration: 2,
+  maxCount: 1
 });
 
 type Props = OwnProps & ContextProps;
 
 class LoginPageComponent extends React.Component<Props, {}> {
+  private static readonly LOGIN_INITIAL_VALUES: LoginValues = { username: "", password: "", checkbox: false };
 
-    private static readonly LOGIN_INITIAL_VALUES: LoginValues = { username: '', password: '', checkbox:false};
+  private static readonly validate = (values: LoginValues): LoginErrors => {
+    const errors: LoginErrors = {};
 
-    private static readonly validate = (values: LoginValues): LoginErrors => {
-        const errors: LoginErrors = {};
-
-        if (!values.username) {
-            errors.username = 'Prisijungimo vardas yra privalomas';
-        }
-        if (!values.password) {
-            errors.password = 'Slaptažodis yra privalomas';
-        }
-
-        return errors;
-    };
-
-    public render(): React.ReactNode {
-        const {
-            authenticated,
-        } = this.props;
-
-        if (authenticated) {
-            navigationService.redirectToDefaultPage();
-            return;
-        }
-
-        return (
-            <Layout>
-                <Content>
-                    <PageContent>
-                        <Row className={styles.loginContainer}>
-                            <Col xl={10} lg={8} md={7} sm={6} xs={0} />
-                            <Col xl={4} lg={8} md={10} sm={12} xs={24}>
-                                <LoginForm
-                                    initialValues={LoginPageComponent.LOGIN_INITIAL_VALUES}
-                                    onSubmit={this.handleSubmit}
-                                    validate={LoginPageComponent.validate}
-                                />
-                            </Col>
-                            <Col xl={10} lg={8} md={7} sm={6} xs={0} />
-                        </Row>
-                    </PageContent>
-                </Content>
-            </Layout>
-        );
+    if (!values.username) {
+      errors.username = "You need to enter a usename";
+    }
+    if (!values.password) {
+      errors.password = "You need to enter a password";
     }
 
-    private readonly handleSubmit = (values: LoginValues, { resetForm }: FormikHelpers<LoginValues>): void => {
-        sessionService.login(values.username, values.password, values.checkbox)
-            .then(() => { navigationService.redirectToDefaultPage(); })
-            .catch(error => this.handleError(error, resetForm,values));
-    };
+    return errors;
+  };
 
-    private readonly handleError = (
-        error: any,
-        resetForm: (nextValues?: Partial<FormikState<LoginValues>>) => void,
-        values: LoginValues,
-    ): void => {
-        values.password=LoginPageComponent.LOGIN_INITIAL_VALUES.password;
-        resetForm({ values});
+  public render(): React.ReactNode {
+    const { authenticated } = this.props;
 
-        const errorMessage: string = error.status === 403
-            ? 'Neteisingas prisijungimo vardas arba slaptažodis'
-            : error.data.message;
+    if (authenticated) {
+      navigationService.redirectToDefaultPage();
+      return;
+    }
 
-        message.error(errorMessage, 3);
+    return (
+      <Layout className={styles.loginLayout}>
+        <Content>
+          <PageContent>
+            <Row className={styles.loginContainer}>
+              <div>
+                <Logo fontSize={"2.5rem"} />
+                <span>Learning Online</span>
+              </div>
+              <LoginForm initialValues={LoginPageComponent.LOGIN_INITIAL_VALUES} onSubmit={this.handleSubmit} validate={LoginPageComponent.validate} />
+            </Row>
+            <img alt="Login User" src={"images/login-user.svg"} className={styles.loginImage} />
+          </PageContent>
+        </Content>
+      </Layout>
+    );
+  }
 
-        loggerService.error(errorMessage, error);
-    };
+  private readonly handleSubmit = (values: LoginValues, { resetForm }: FormikHelpers<LoginValues>): void => {
+    sessionService
+      .login(values.username, values.password, values.checkbox)
+      .then(() => {
+        navigationService.redirectToDefaultPageAfterLogin();
+      })
+      .catch(error => this.handleError(error, resetForm, values));
+  };
 
+  private readonly handleError = (error: any, resetForm: (nextValues?: Partial<FormikState<LoginValues>>) => void, values: LoginValues): void => {
+    values.password = LoginPageComponent.LOGIN_INITIAL_VALUES.password;
+    resetForm({ values });
+
+    const errorMessage: string = error.status === 403 ? "Incorrect username or password" : error.data.message;
+
+    message.error(errorMessage, 3);
+
+    loggerService.error(errorMessage, error);
+  };
 }
 
 const mapContextToProps = ({ session: { authenticated } }: SettingsProps): ContextProps => ({
-    authenticated,
+  authenticated
 });
 
 const LoginPage = connectContext(mapContextToProps)(LoginPageComponent);
