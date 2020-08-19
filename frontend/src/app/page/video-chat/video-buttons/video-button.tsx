97f733b0c590ff464462f48574286d256b6f72a9
unknown <eglegustaite@gmail.com>
2020-08-19 13:15:18 +0300

    sider to right

--- frontend/src/app/page/video-chat/video-buttons/video-button.tsx
+++ frontend/src/app/page/video-chat/video-buttons/video-button.tsx
@@ -5,11 +5,22 @@ import {Wand} from "../wand"
 
 import styles from './video-buttons.module.scss'
 
-const VideoButton: React.FC = () =>(
+interface Props {
+    role: string[];
+}
+
+const VideoButton: React.FC<Props> = ({role}) =>(
     <div>
-        <div className={styles.videobtn} ><Button type='primary' style={{borderRadius: '100%', height: '45px'}}><TeamOutlined /></Button> Participants</div>
-        <div className={styles.videobtn}><Button type='primary'  style={{borderRadius: '100%', height: '45px'}}><MessageOutlined /></Button> Question Form</div>
-        <div className={styles.videobtn}><Button type='primary'  style={{borderRadius: '100%', height: '45px'}}><span style={{ width: '15px', display:'flex' }}><Wand /></span></Button> Whiteboard</div>
+        <div className={styles.videobtn} style={{cursor: 'pointer'}}><Button type='primary' style={{borderRadius: '100%', height: '45px'}}><TeamOutlined /></Button> Participants</div>
+        {
+            (role[0] === "STUDENT") ? console.log('student') :
+            (
+                <>
+                    <div className={styles.videobtn}><Button type='primary'  style={{borderRadius: '100%', height: '45px'}}><MessageOutlined /></Button> Question Form</div>
+                    <div className={styles.videobtn}><Button type='primary'  style={{borderRadius: '100%', height: '45px'}}><span style={{ width: '15px', display:'flex' }}><Wand /></span></Button> Whiteboard</div>
+                </>
+            )
+        }
     </div>
 )
 
