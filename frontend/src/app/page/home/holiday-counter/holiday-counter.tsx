57cffe57b8231395af5b7e31f28be81b729592e7
Romanas <go.romanas@gmail.com>
2020-08-24 19:37:31 +0300

    Added holiday counter until Christmas

--- frontend/src/app/page/home/holiday-counter/holiday-counter.tsx
+++ frontend/src/app/page/home/holiday-counter/holiday-counter.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import styles from './holiday-counter.module.scss';
 
-const time = Math.round((Date.parse(new Date('2020-12-24')) - Date.parse(new Date())) / 1000 / 86400);
+// const time = Math.round((Date.parse(new Date('2020-12-24')) - Date.parse(new Date())) / 1000 / 86400);
+const deadline = new Date('2020-12-24');
+const today = new Date();
+const time = Math.round(Math.abs((deadline.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)));
 
 const HolidayCounter: React.FC = () =>
 
     (
-        <div className={styles.holidayCounter}>
+        <div>
             <img
                 alt="Teacher board"
                 src={'images/teacher-board.svg'}
